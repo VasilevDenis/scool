9c22c5e9b3daabf06cad241941d28876b37959ad
Denis Vasilev <pythocoder@gmail.com>
2023-08-13 03:32:52 +0600

    ordering

--- school/views.py
+++ school/views.py
@@ -6,11 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    students = Student.objects.order_by('name')  # Retrieve data from your model
+    ordering = 'group'
+    students = Student.objects.order_by(ordering)  # Retrieve data from your model
     context = {'students': students}  # Create a dictionary with the data
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
     return render(request, template, context)
